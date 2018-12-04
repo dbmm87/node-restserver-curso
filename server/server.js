c923b34a4a73fa3b01c61ef43b92f5ee2fde746e
Demetrio Méndez <demetrio@inventmx.com>
2018-12-04 17:35:21 -0600

    version uno

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
